134ce01c7e5eafe1b3480b0e56324e620afec784
seb-faull <seb_faull@hotmail.co.uk>
2017-10-05 19:09:15 +0100

    Added html to all views

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -34,28 +34,11 @@ class TodosController < ApplicationController
     current_user.todos.destroy(params[:id])
     redirect_to todos_url
   end
+
+  protected
+  def todo_params
+    params.require(:todo).permit(:title, :body) #:user_id
+  end
+
 end
 
-  # create_table "todos", force: :cascade do |t|
-  #   t.string "title"
-  #   t.text "body"
-  #   t.integer "user_id"
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  # end
-
-  # create_table "users", force: :cascade do |t|
-  #   t.string "email", default: "", null: false
-  #   t.string "encrypted_password", default: "", null: false
-  #   t.string "reset_password_token"
-  #   t.datetime "reset_password_sent_at"
-  #   t.datetime "remember_created_at"
-  #   t.integer "sign_in_count", default: 0, null: false
-  #   t.datetime "current_sign_in_at"
-  #   t.datetime "last_sign_in_at"
-  #   t.inet "current_sign_in_ip"
-  #   t.inet "last_sign_in_ip"
-  #   t.datetime "created_at", null: false
-  #   t.datetime "updated_at", null: false
-  #   t.index ["email"], name: "index_users_on_email", unique: true
-  #   t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
