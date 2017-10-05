187559936a11dc592c93018fd7a5edc808653780
seb-faull <seb_faull@hotmail.co.uk>
2017-10-05 17:00:35 +0100

    Authenticate user in application controller

---
+++ db/migrate/20171005155241_create_todos.rb
@@ -0,0 +1,11 @@
+class CreateTodos < ActiveRecord::Migration[5.1]
+  def change
+    create_table :todos do |t|
+      t.string :title
+      t.text :body
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
